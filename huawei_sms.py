398c5712c30fd234d8410b08a5860c11f603f4fd
Sergey Melnikov <svembox@gmail.com>
2020-05-02 19:53:42 +0300

    Add telegram support to huawei_sms script

--- huawei_sms.py
+++ huawei_sms.py
@@ -1,15 +1,42 @@
+#!/usr/bin/python3
+
+import json
+import urllib
+
 from huawei_lte_api.AuthorizedConnection import AuthorizedConnection
 from huawei_lte_api.Client import Client
 from huawei_lte_api.enums.sms import BoxTypeEnum
 
-connection = AuthorizedConnection("http://192.168.2.1", "admin", "admin")
+from config import *
+
+connection = AuthorizedConnection(f'http://{modem_ip}', modem_username, modem_password)
 client = Client(connection)
-print(client.sms.get_sms_list())
-sms = client.sms.get_sms_list(1, BoxTypeEnum.LOCAL_INBOX, 1, 0, 0, 1)
-if sms['Messages'] is None:
-    exit(0)
-print(sms)
-print('SMS from ' + sms['Messages']['Message']['Phone'])
-print("Date:" + sms['Messages']['Message']['Date'])
-print("Content: " + sms['Messages']['Message']['Content'])
-client.sms.set_read()
+
+while True:
+    sms = client.sms.get_sms_list(1, BoxTypeEnum.LOCAL_INBOX, 1, 0, 0, 1)
+    if sms['Messages'] is None:
+        exit(0)
+    print(sms)
+    sender = sms['Messages']['Message']['Phone']
+    date = sms['Messages']['Message']['Date']
+    text = sms['Messages']['Message']['Content']
+    sms_index = sms['Messages']['Message']['Index']
+    unread = sms['Messages']['Message']['Smstat'] == '0'
+    notification = f'SMS from {sender}\nDate: {date}\n{text}'
+    print(notification)
+    print(f'is_unread = {unread}')
+    if not unread:
+        break
+
+    url = f'https://api.telegram.org/bot{telegram_token}/sendMessage'
+
+    data = {'chat_id': telegram_chat_id, 'text': notification}
+    data = urllib.parse.urlencode(data).encode()
+
+    req = urllib.request.Request(url, data=data)
+    response = json.loads(urllib.request.urlopen(req).read())
+    print(response)
+    if response['ok']:
+        client.sms.set_read(sms_index)
+    else:
+        break
