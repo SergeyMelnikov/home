42049844cf4cee56c0c1b6ea5e3556f6dbebd197
Sergey Melnikov <svembox@gmail.com>
2021-12-21 13:25:18 +0300

    Old changes

--- play_awair_status.sh
+++ play_awair_status.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./googlehome_say.py 192.168.88.11 ru "$(curl -s http://192.168.88.15/air-data/latest | ./awair_status.py)"
+./googlehome_say.py 192.168.99.5 ru "$(curl -s http://awair/air-data/latest | ./awair_status.py)"
