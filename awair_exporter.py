8be7525c57ea43feb1b38395cb49381c0f1d3dc9
Sergey Melnikov <svembox@gmail.com>
2023-04-26 12:44:06 +0200

    Support multiple awairs

--- awair_exporter.py
+++ awair_exporter.py
@@ -5,59 +5,74 @@ import json
 import sys
 import urllib
 
+from pprint import pprint
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
 
+
+class Awair:
+    def __init__(self, ip, name, model):
+        self.ip = ip
+        self.name = name
+        self.model = model
+
+
+config = [
+    Awair("192.168.93.11", "Bedroom", "awair-r2"),
+    Awair("192.168.93.10", "Office", "awair-element"),
+]
+
+
 class AwairCollector(object):
 
-    def __init__(self, ip):
-        self.ip = ip
-        self.device_config = json.loads(urllib.request.urlopen(f'http://{self.ip}/settings/config/data').read())
-        print(self.device_config, flush=True)
+    def __init__(self, config):
+        self.devices = config
+        for device in self.devices:
+            device.device_config = json.loads(urllib.request.urlopen(f'http://{device.ip}/settings/config/data').read())
+            pprint(device.device_config)
 
     def collect(self):
-        #        try:
-        data = json.loads(urllib.request.urlopen(f'http://{self.ip}/air-data/latest').read())
-        print(str(datetime.datetime.now()) + " " + str(data), flush=True)
+#      try:
+        for device in self.devices:
+            data = json.loads(urllib.request.urlopen(f'http://{device.ip}/air-data/latest').read())
+            print(str(datetime.datetime.now()) + " " + str(data), flush=True)
 
-        for metric, value in data.items():
-            if metric in ['timestamp']:
-                continue
-            g = GaugeMetricFamily(
-                'awair_sensor_' + metric,
-                'Awair sensor data for ' + metric,
-                labels=['device_uuid']
-            )
-            g.add_metric(value=value, labels=[self.device_config['device_uuid']])
-            yield g
-            NEW_NAME = {
-                'temp': 'temperature',
-                'humid': 'humidity'
-            }
-            metric = NEW_NAME.get(metric, metric)
-            g = GaugeMetricFamily(
-                'sensor_' + metric,
-                'sensor data for ' + metric,
-                labels=['device_uuid', 'device_name', 'device_type']
-            )
-            g.add_metric(value=value, labels=[self.device_config['device_uuid'], 'Bedroom', 'awair-r2'])
-            yield g
-
-
-
-
-#        except Exception as e:
-#            print(e)
-
+            for metric, value in data.items():
+                if metric in ['timestamp']:
+                    continue
+                g = GaugeMetricFamily(
+                    'awair_sensor_' + metric,
+                    'Awair sensor data for ' + metric,
+                    labels=['device_uuid']
+                )
+                g.add_metric(value=value, labels=[device.device_config['device_uuid']])
+                #print(g)
+                yield g
+                NEW_NAME = {
+                    'temp': 'temperature',
+                    'humid': 'humidity'
+                }
+                metric = NEW_NAME.get(metric, metric)
+                g = GaugeMetricFamily(
+                    'sensor_' + metric,
+                    'sensor data for ' + metric,
+                    labels=['device_uuid', 'device_name', 'device_type']
+                )
+                g.add_metric(value=value, labels=[device.device_config['device_uuid'], device.name, device.model])
+                #print(g)
+                yield g
+#      except Exception as e:
+#        print(e)
+#      else:
+#        print("ok")
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: python3 await_exporter.py <ip of Awair with local sensors enabled> "
-              "[<port to export varz, default 8000>]")
+    if len(sys.argv) < 1:
+        print("Usage: python3 await_exporter.py [<port to export varz, default 8000>]")
         sys.exit(1)
-    REGISTRY.register(AwairCollector(sys.argv[1]))
-    if len(sys.argv) > 2:
+    REGISTRY.register(AwairCollector(config))
+    if len(sys.argv) > 1:
         port = int(sys.argv[2])
     else:
         port = 8000
