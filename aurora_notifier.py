5472b5995cd90e96f5f2fc1934b09586c1b8d06c
Sergey Melnikov <sergey@neon.tech>
2023-12-01 19:34:06 +0100

    Support polar night

--- aurora_notifier.py
+++ aurora_notifier.py
@@ -9,33 +9,50 @@ import humanize
 
 import telegram
 
+KPI_THRESHOLD = 6
+CLOUD_THRESHOLD = 50
 userAgent = "playground sergey@melnikov.no"
-# Oslo
+city = "Oslo"
 latitude = 59.948
 longitude = 10.767
-# Kirkenes
+
+# city = "Kirkenes"
 # latitude = 69.73
 # longitude = 30.05
 now = datetime.utcnow().replace(tzinfo=timezone.utc)
 metno_url = f"https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={latitude}&lon={longitude}"
-# metno_url = "https://melnikov.no/metno.json"
 kpi_url = "https://services.swpc.noaa.gov/products/noaa-planetary-k-index-forecast.json"
-# kpi_url = "https://melnikov.no/noaa-planetary-k-index-forecast.json"
+
 
 sun_today_url = f"https://api.met.no/weatherapi/sunrise/3.0/sun?lat={latitude}&lon={longitude}&date={now.date()}"
 sun_tomorrow_url = f"https://api.met.no/weatherapi/sunrise/3.0/sun?lat={latitude}&lon={longitude}&date={(now + timedelta(days=1)).date()}"
+sunset_str = json.loads(urllib.request.urlopen(
+    urllib.request.Request(sun_today_url, headers={'User-Agent': userAgent})).read()
+                        )["properties"]["sunset"]["time"]
+sunrise_str = json.loads(urllib.request.urlopen(
+    urllib.request.Request(sun_tomorrow_url, headers={'User-Agent': userAgent})).read()
+                         )["properties"]["sunrise"]["time"]
 try:
-    sunset = datetime.fromisoformat(json.loads(urllib.request.urlopen(
-        urllib.request.Request(sun_today_url, headers={'User-Agent': userAgent})).read()
-                                               )["properties"]["sunset"]["time"])
+    sunset = datetime.fromisoformat(sunset_str)
+    sunrise = datetime.fromisoformat(sunrise_str)
 except:
-    sunset = datetime.fromisoformat("2023-11-26T11:19:00+01:00")
-try:
-    sunrise = datetime.fromisoformat(json.loads(urllib.request.urlopen(
-        urllib.request.Request(sun_tomorrow_url, headers={'User-Agent': userAgent})).read()
-                                                )["properties"]["sunrise"]["time"])
-except:
-    sunrise = datetime.fromisoformat("2024-01-16T10:44:00+01:00")
+    sunset = sunrise = None
+    # sunset = datetime.fromisoformat("2023-11-26T11:19:00+01:00")
+    # sunrise = datetime.fromisoformat("2024-01-16T10:44:00+01:00")
+
+def timeDescription(time):
+    if sunset:
+        return (f"{humanize.naturaldelta(time - sunset)} after sunset, "
+                f"{humanize.naturaldelta(sunrise - time)} before sunrise. ")
+    else:
+        return "Polar night. "
+
+def isNighttime(time):
+    if sunset:
+        return sunset < time < sunrise
+    else:
+        return True
+
 
 met_data = json.loads(urllib.request.urlopen(
     urllib.request.Request(metno_url, headers={'User-Agent': userAgent})).read()
@@ -53,28 +70,29 @@ def getClouds(time: datetime):
     return float("nan")
 
 
-data = json.loads(urllib.request.urlopen(kpi_url).read())[1:]
+kpi_data = json.loads(urllib.request.urlopen(kpi_url).read())[1:]
 tz_oslo = zoneinfo.ZoneInfo("Europe/Oslo")
 
 messages = []
-for row in data:
+for row in kpi_data:
     time = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
 
-    if time < now - timedelta(hours=0) or time > now + timedelta(hours=12):
+    if time < now - timedelta(hours=0) or time > now + timedelta(hours=24):
         continue
-    if time < sunset or time > sunrise:
+    if not isNighttime(time):
         continue
     kpi = float(row[1])
     cloudsPercent = getClouds(time)
-    if kpi >= 6 and cloudsPercent < 50:
+    # print(time, kpi, cloudsPercent)
+    if kpi >= KPI_THRESHOLD and cloudsPercent < CLOUD_THRESHOLD:
         message = (
-            f"Kp {kpi} is expected in {humanize.naturaltime(now - time)}, "
-            f"{humanize.naturaldelta(time - sunset)} after sunset, "
-            f"{humanize.naturaldelta(sunrise - time)} before sunrise. "
-            f"Clouds will be at {cloudsPercent}%. "
+                f"Kp {kpi} is expected in {humanize.naturaltime(now - time)}, " +
+                timeDescription(time) +
+                f"Clouds will be at {cloudsPercent}%. "
         )
         messages.append(message)
 
 if messages:
+    messages.append(f"at {city}")
     print(messages)
     telegram.sendMessage("\n".join(messages))
